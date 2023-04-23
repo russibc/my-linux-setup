bdc1196c319a97014201d329887b0a5f5c35937d
russibc <bruna.russi@ifsc.edu.br>
2023-04-23 14:54:29 -0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
-echo "Starting configuration..."
+echo "Starting packages install..."
 
-# Remove unwanted packages
-echo "Removing unwanted packages..."
-sudo dnf remove eog -y
-sudo dnf remove evince -y
-sudo dnf remove rhythmbox -y
-sudo dnf remove libreoffice* -y
-sudo dnf remove totem -y
-sudo dnf remove gnome-maps -y
-sudo dnf remove gnome-contacts -y
-sudo dnf remove gnome-weather -y
-sudo dnf remove gnome-boxes -y
-sudo dnf remove gnome-photos -y
-
-# Clean dnf cache, update, and upgrade packages
-echo "Cleaning dnf cache, updating, and upgrading packages..."
-sudo dnf clean all && sudo dnf update -y && sudo dnf upgrade -y
-
-# Install RPM Fusion repository
-echo "Installing RPM Fusion repository..."
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-# Update core group
-echo "Updating Fedora's core group..."
-sudo dnf group update core -y
-
-# Install dev tools
-echo "Install dev tools..."
+# Install OpenJDK, PyQt5, Qt Designer, Node.js and npm
+echo "Installing OpenJDK, PyQt5, Qt Designer, Node.js and npm..."
 sudo dnf install java-latest-openjdk.x86_64 -y
 sudo dnf install pip PyQt5 qt5-designer.x86_64 -y
 sudo dnf install nodejs npm -y
+
+# Install Development Tools group
+echo "Installing Development Tools group..."
 sudo dnf groupinstall 'Development Tools' -y
 
 # Install additional packages
-echo "Installing graphical packages..."
+echo "Installing additional packages..."
 sudo dnf install gthumb kdenlive fritzing simplescreenrecorder drawing vlc audacity inkscape gimp -y
 
 # Install system packages
-echo "Installing utilitaries packages..."
+echo "Installing system packages..."
 sudo dnf install gnome-extensions-app xkill okular gparted gnome-tweak-tool alacarte transmission -y 
+
+# Install Steam
+echo "Installing Steam..."
+sudo dnf install steam -y
+
+# Install Spotify
+echo "Installing Spotify..."
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo -y
+sudo dnf install spotify-client -y
+
+# Install GitKraken
+echo "Installing GitKraken..."
+wget https://release.gitkraken.com/linux/gitkraken-amd64.rpm -P /tmp/
+sudo dnf install /tmp/gitkraken-amd64.rpm -y
+
+# Install Visual Studio Code
+echo "Installing Visual Studio Code..."
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo tee /etc/yum.repos.d/vscode.repo <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install code -y
 
 echo "Configuration completed."
